374406009e6962fa16e2a551396ace0990491911
YongSeok <qazwsx1455@gmail.com>
2020-10-14 17:22:11 +0900

    commit2

---
+++ src/Hello.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// const Hello = ()=> React.createElement(
+//     'p',
+//     {className: 'hello'},
+//     'hello, world!'
+// )
+
+const Hello =()=>{
+    return (
+        <section className="box">
+            <h1>Message</h1>
+            <p className='hello'>Hello, world!!!</p>
+        </section>
+    )
+}
+
+export default Hello;
