95c08079f796f2969761bd377d1f4b3554661070
YongSeok <qazwsx1455@gmail.com>
2020-10-27 17:17:32 +0900

    create add inptut

--- client/src/pages/Board.js
+++ client/src/pages/Board.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import TaskAdd from '../components/TaskAdd';
 
 const Board = () =>{
     return(
         <div>
             <h2>
                 여기는 게시판
+                <TaskAdd/>
             </h2>
         </div>
     );
