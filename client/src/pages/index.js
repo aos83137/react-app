163dd23bcb00b192d64d7eac60b4efbf65676da0
YongSeok <qazwsx1455@gmail.com>
2020-10-28 14:02:41 +0900

    App.js 수정, route문제 해결

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,2 +1,3 @@
 export { default as Home } from './Home';
-export { default as Login } from './Board';
+export { default as Login } from './Login';
+export { default as Board } from './Board';
--- client/src/shared/App.js
+++ client/src/shared/App.js
@@ -1,24 +1,212 @@
 import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Icon from '@material-ui/core/Icon';
+
 import {  
     BrowserRouter as Router,
     Switch,
     Route,
-    // Link
+    // useRouteMatch,
+    Link
 } from 'react-router-dom';
-import {Home} from '../pages';
+import {Home,Login,Board} from '../pages';
 
-const App=()=>{
-    
-    return (
-        <Router>
+// import { firestore } from "../firebase";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  linkType:{
+    textDecoration:'none',
+    color: "#000000",
+  },
+  linkLogin:{
+    textDecoration:'none',
+    color: "#FFFFFF",
+  }
+}));
+
+export default function App() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const goLogin=()=>{
+      
+  }
+  const goHome=()=>{
+
+  }
+  const goBoard=()=>{
+
+  }
+  return (
+    <Router>
+      <div className={classes.root}>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          className={clsx(classes.appBar, {
+            [classes.appBarShift]: open,
+          })}
+        >
+          <Toolbar>
+              <IconButton
+                  color="inherit"
+                  aria-label="open drawer"
+                  onClick={handleDrawerOpen}
+                  edge="start"
+                  className={clsx(classes.menuButton, open && classes.hide)}
+              >
+                  <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                  리엑트와 파이어베이스로 만드는 홈페이지
+              </Typography>
+              <Link to="/login" className={classes.linkLogin}>
+                <IconButton
+                    color="inherit"
+                    aria-label="login"
+                    edge="end"
+                    onClick={goLogin}
+                >
+                    <Icon>login</Icon>
+                </IconButton>
+              </Link>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          className={classes.drawer}
+          variant="persistent"
+          anchor="left"
+          open={open}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+              <Link to="/" className={classes.linkType}>
+                  <ListItem button key={"홈"} onClick={goHome}>
+                      <ListItemIcon>
+                          <Icon>home</Icon>
+                      </ListItemIcon>
+                      <ListItemText primary={"홈"} />
+                  </ListItem>
+              </Link>
+              <Link to="/board" className={classes.linkType}>
+                  <ListItem button key={"게시판"} onClick={goBoard}>
+                      <ListItemIcon>
+                          <Icon>list_alt</Icon>
+                      </ListItemIcon>                
+                      <ListItemText primary={"게시판"} />
+                  </ListItem>
+              </Link>
+          </List>
+          <Divider />
+
+        </Drawer>
+        <main
+          className={clsx(classes.content, {
+            [classes.contentShift]: open,
+          })}
+        >
+          <div className={classes.drawerHeader} />
             <Switch>
                 <div>
-                    <Route path="/" component={Home}/>
-                    {/* <Route path="/login" component={Login}/> */}
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/Board" component={Board}/>
                 </div>
             </Switch>
-        </Router>
-    );
+        </main>
+      </div>
+    </Router>
+  );
 }
-
-export default App;
