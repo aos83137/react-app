19614c6c15e57cf5d1df6c45f0520d38648567db
YongSeok <qazwsx1455@gmail.com>
2020-10-29 11:42:37 +0900

    create card view

--- client/src/pages/Board.js
+++ client/src/pages/Board.js
@@ -1,13 +1,100 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from "react";
 import TaskAdd from '../components/TaskAdd';
+import TaskDisplay from '../components/TaskDisplay';
+
+import {firestore} from "../firebase";
 
 const Board = () =>{
+    const [tasks, setTasks] = useState([]);
+    const [task, setTask] = useState("");
+    const [modify, setModify] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const fetchData = useCallback(() => {
+        let tasksData = [];
+        setLoading(true);
+        firestore
+          .collection("tasks")
+          .get()
+          .then((docs) => {
+            docs.forEach((doc) => {
+              console.log('doc',doc.data(), doc.id);
+              tasksData.push({ todo: doc.data().todo, id: doc.id });
+            });
+            setTasks((prevTasks) => prevTasks.concat(tasksData));
+            setLoading(false);
+          });
+      }, []);
+    useEffect(() => {
+        fetchData();
+        }, [fetchData]);
+
+    const onClickHandler = (e) => {
+        e.preventDefault();
+        if (task !== "") {
+            firestore
+            .collection("tasks")
+            .add({ todo: task })
+            .then((res) => {
+                console.log(res);
+                setTasks((prevTasks) => tasks.concat({ todo: task, id: res.id }));
+            });
+            setTask("");
+        }
+    };
+
+    const onChangeHandler = (e) => {
+        setTask(e.target.value);
+    };
+
+    const removeHandler = (id) => {
+        firestore
+            .collection("tasks")
+            .doc(id)
+            .delete()
+            .then(() =>
+            setTasks((prevTasks) =>
+                prevTasks.filter((prevTask) => id !== prevTask.id)
+            )
+        );
+    };
+
+    const modifyHandler = (id) => {
+        return (e) => {
+            setTask("");
+            setModify(true);
+            e.target.innerText = modify ? "수정" : "완료";
+            if (task !== "" && modify) {
+            setTasks((prevTasks) =>
+                tasks.map((taskOne) =>
+                taskOne.id === id ? { ...taskOne, todo: task } : taskOne
+                )
+            );
+            setTask("");
+            setModify(false);
+            }
+        };
+    };
+    
     return(
         <div>
             <h2>
                 여기는 게시판
-                <TaskAdd/>
             </h2>
+            <TaskAdd
+                task={task}
+                onClickHandler={onClickHandler}
+                onChangeHandler={onChangeHandler}
+                modify={modify}
+            />
+            {loading && <h1>Loading ...</h1>}
+            {!loading && (
+                <TaskDisplay
+                tasks={tasks}
+                removeHandler={removeHandler}
+                modifyHandler={modifyHandler}
+                />
+            )}
         </div>
     );
 }
