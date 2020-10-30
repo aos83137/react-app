5586d0dca752906c69a32c2dfca77ba6add10f81
YongSeok <qazwsx1455@gmail.com>
2020-10-30 17:51:08 +0900

    10.30

--- client/src/components/TaskDisplay.jsx
+++ client/src/components/TaskDisplay.jsx
@@ -82,7 +82,7 @@ const TaskDisplay = ({ boards, removeHandler, modifyHandler }) => {
         <CardHeader
           avatar={
             <Avatar aria-label="recipe" className={classes.avatar}>
-              R
+              {board.whose[0]}
             </Avatar>
           }
           action={
@@ -100,8 +100,7 @@ const TaskDisplay = ({ boards, removeHandler, modifyHandler }) => {
         />
         <CardContent>
           <Typography variant="body2" color="textSecondary" component="p">
-            This impressive paella is a perfect party dish and a fun meal to cook together with your
-            guests. Add 1 cup of frozen peas along with the mussels, if you like.
+            {board.cardContent}
           </Typography>
         </CardContent>
         <CardActions disableSpacing>
@@ -124,18 +123,8 @@ const TaskDisplay = ({ boards, removeHandler, modifyHandler }) => {
         </CardActions>
         <Collapse in={expanded} timeout="auto" unmountOnExit>
           <CardContent>
-            <Typography paragraph>Method:</Typography>
             <Typography paragraph>
-              Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-              minutes.
-            </Typography>
-            <Typography paragraph>
-              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-              heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-              browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-              and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-              pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-              saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+              {board.content}
             </Typography>
             <Typography paragraph>
               Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
--- client/src/pages/Board.js
+++ client/src/pages/Board.js
@@ -5,7 +5,7 @@ import TaskDisplay from '../components/TaskDisplay';
 import {firestore} from "../firebase";
 
 const Board = () =>{
-    const [tasks, setTasks] = useState([]);
+    // const [tasks, setTasks] = useState([]);
     const [task, setTask] = useState("");
     const [boards, setBoards] = useState([]);
 
@@ -13,20 +13,20 @@ const Board = () =>{
     const [loading, setLoading] = useState(false);
 
     const fetchData = useCallback(() => {
-        let tasksData = [];
+        // let tasksData = [];
         let boardData = [];
 
         setLoading(true);
-        firestore
-          .collection("tasks")
-          .get()
-          .then((docs) => {
-            docs.forEach((doc) => {
-              console.log('doc',doc.data(), doc.id);
-              tasksData.push({ todo: doc.data().todo, id: doc.id });
-            });
-            setTasks((prevTasks) => prevTasks.concat(tasksData));
-          });
+        // firestore
+        //   .collection("tasks")
+        //   .get()
+        //   .then((docs) => {
+        //     docs.forEach((doc) => {
+        //       console.log('doc',doc.data(), doc.id);
+        //       tasksData.push({ todo: doc.data().todo, id: doc.id });
+        //     });
+        //     setTasks((prevTasks) => prevTasks.concat(tasksData));
+        //   });
 
         firestore
           .collection("boards")
@@ -51,49 +51,49 @@ const Board = () =>{
 
     const onClickHandler = (e) => {
         e.preventDefault();
-        if (task !== "") {
-            firestore
-            .collection("tasks")
-            .add({ todo: task })
-            .then((res) => {
-                console.log(res);
-                setTasks((prevTasks) => tasks.concat({ todo: task, id: res.id }));
-            });
-            setTask("");
-        }
+        // if (task !== "") {
+        //     firestore
+        //     .collection("tasks")
+        //     .add({ todo: task })
+        //     .then((res) => {
+        //         console.log(res);
+        //         setTasks((prevTasks) => tasks.concat({ todo: task, id: res.id }));
+        //     });
+        //     setTask("");
+        // }
     };
 
     const onChangeHandler = (e) => {
-        setTask(e.target.value);
+        // setTask(e.target.value);
     };
 
     const removeHandler = (id) => {
-        firestore
-            .collection("tasks")
-            .doc(id)
-            .delete()
-            .then(() =>
-            setTasks((prevTasks) =>
-                prevTasks.filter((prevTask) => id !== prevTask.id)
-            )
-        );
+        // firestore
+        //     .collection("tasks")
+        //     .doc(id)
+        //     .delete()
+        //     .then(() =>
+        //     setTasks((prevTasks) =>
+        //         prevTasks.filter((prevTask) => id !== prevTask.id)
+        //     )
+        // );
     };
 
     const modifyHandler = (id) => {
-        return (e) => {
-            setTask("");
-            setModify(true);
-            e.target.innerText = modify ? "수정" : "완료";
-            if (task !== "" && modify) {
-            setTasks((prevTasks) =>
-                tasks.map((taskOne) =>
-                taskOne.id === id ? { ...taskOne, todo: task } : taskOne
-                )
-            );
-            setTask("");
-            setModify(false);
-            }
-        };
+        // return (e) => {
+        //     setTask("");
+        //     setModify(true);
+        //     e.target.innerText = modify ? "수정" : "완료";
+        //     if (task !== "" && modify) {
+        //     setTasks((prevTasks) =>
+        //         tasks.map((taskOne) =>
+        //         taskOne.id === id ? { ...taskOne, todo: task } : taskOne
+        //         )
+        //     );
+        //     setTask("");
+        //     setModify(false);
+        //     }
+        // };
     };
     
     return(
