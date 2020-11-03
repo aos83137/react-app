24e87dac6ad9a7c91c8ed2c4da71641f363dfff4
YongSeok <qazwsx1455@gmail.com>
2020-11-03 23:17:26 +0900

    commit

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,7 @@ const Home =()=>{
     return (
         <div>
             <h2>Home</h2>
+            <h3>안녕하세요 ~~ 고객님</h3>
         </div>
     );
 }
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,41 @@
 import React,{useState} from 'react';
-import { Redirect } from "react-router-dom"
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import Lock from '@material-ui/icons/Lock';
+import Button from "@material-ui/core/Button";
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import { Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    margin: {
+      margin: theme.spacing(1),
+    },
+    mainContainer:{
+        margin:theme.spacing(20),
+        padding:theme.spacing(10),
+        backgroundColor:"white",
+        minWidth:"580px"
+    },
+    gridStyle:{
+        fontSize:"24px",
+        marginBottom:"15spx",
+    },
+    buttonFom:{
+        marginTop:theme.spacing(1),
+    }
+  }));
 
 const Login =()=>{
+    const classes = useStyles();
+
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-
+    
     const handleClick=()=>{
         try{
             Login({email, password})
@@ -14,26 +45,53 @@ const Login =()=>{
             setPassword("")
         }
     }
-    // const { from } = location.state || { from: { pathname: "/" } }
-    // if (authenticated) return <Redirect to={from} />
 
     return (
-        <>
-            <h1>Login</h1>
-            <input
-            value={email}
-            onChange={({ target: { value } }) => setEmail(value)}
-            type="text"
-            placeholder="email"
-            />
-            <input
-            value={password}
-            onChange={({ target: { value } }) => setPassword(value)}
-            type="password"
-            placeholder="password"
-            />
-            <button onClick={handleClick}>Login</button>
-        </>
+        <Card className={classes.mainContainer}>
+            <Grid container spacing={3} alignItems="center" justify="center" direction="column">
+                <Grid>
+                    <Typography className={classes.gridStyle}>
+                        로그인
+                    </Typography>
+                </Grid>
+                <Grid container spacing={1} direction="row" alignItems="center" justify="center" >
+                    <Grid item>
+                    <AccountCircle />
+                    </Grid>
+                    <Grid item>
+                    <TextField id="input-with-icon-grid" label="Id" />
+                    </Grid>
+                </Grid>
+                <Grid container spacing={1} direction="row" alignItems="center" justify="center" >
+                    <Grid item>
+                    <Lock/>
+                    </Grid>
+                    <Grid item>
+                    <TextField id="input-with-icon-grid" label="Password" />
+                    </Grid>
+                </Grid>
+                <Grid className={classes.buttonFom} container spacing={2} alignItems="center" justify="center" >
+                    <Grid item>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            // onClick={}
+                        >
+                            {"회원가입"}
+                        </Button>
+                    </Grid>
+                    <Grid item>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            // onClick={}
+                        >
+                            {"로그인"}
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Card>
     );
 }
 
