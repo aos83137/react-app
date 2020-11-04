1dd936bd13fcf34960ca0e0f9286dcf8dcd1a9d4
YongSeok <qazwsx1455@gmail.com>
2020-11-04 23:22:04 +0900

    홈 화면 세션에 따라 내용 변환. --home.js 페이지 추가 할 것: 그냥 이쁜 애니메이션 하나 가져와보자 ㅋㅋ

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -18,8 +18,15 @@ const Home =()=>{
         <div>
             {console.log('user',auth)}
             <h2>Home</h2>
-            <h3>안녕하세요 {auth?auth:"~~"} 고객님</h3>
-            
+            {
+                auth?
+                <h3>안녕하세요 {auth} 고객님</h3>
+                :
+                <>
+                    <h3>로그인을 해주세요</h3>
+                    <h3>로그인 시 게시판이 사용가능합니다.</h3>
+                </>
+            }           
         </div>
     );
 }
