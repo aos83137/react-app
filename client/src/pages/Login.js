4d4ef13d1d84c94932f9bc32d1dfbd50eb42ce05
YongSeok <qazwsx1455@gmail.com>
2020-11-09 17:02:51 +0900

    추후 할일: 회원가입 페이지 만들기

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -76,7 +76,25 @@ const Login =()=>{
     }
 
     const goSignUpClick=(email,password)=>{
-        authService.createUserWithEmailAndPassword(email, password).catch(function(error) {
+        authService.createUserWithEmailAndPassword(email, password)
+        .then((res)=>{
+            //일단 여기는 email, password를 기반으로 회원가입이 완료됨
+            //하지만 displayName이라던가 세부사항은 못넣어서
+            //res.user.updateProfile을 이용해서 displayName등을 저장해줄 거임.
+            //게시판 부분좀 만들고 이부분 회원가입 페이지 다시좀 만들어 보자구
+            res.user.updateProfile({
+                displayName:"YongSEESEO",
+                photoURL:"https://example.com/jane-q-user/profile.jpg"
+            }).then(function() {
+            // Update successful.
+            //
+                console.log('in Login res:',res.user.email);
+                console.log('in Login res.user.displayName:',res.user.displayName);
+            }).catch(function(error) {
+            // An error happened.
+            });
+        })
+        .catch(function(error) {
             // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
